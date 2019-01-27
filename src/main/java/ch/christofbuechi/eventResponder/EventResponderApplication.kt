e34746b320fed4a813f3f61e29b93e985aa5e610
Christof Buechi <christof.buechi@gmail.com>
2019-01-27 01:05:18 +0100

    add fileWatcher

--- src/main/java/ch/christofbuechi/eventResponder/EventResponderApplication.kt
+++ src/main/java/ch/christofbuechi/eventResponder/EventResponderApplication.kt
@@ -1,17 +1,28 @@
 package ch.christofbuechi.eventResponder
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.integration.channel.PublishSubscribeChannel
+import org.springframework.integration.dsl.IntegrationFlows
+import org.springframework.integration.file.dsl.Files
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHandler
 import org.springframework.web.reactive.HandlerMapping
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
 import org.springframework.web.reactive.socket.WebSocketHandler
+import org.springframework.web.reactive.socket.WebSocketMessage
+import org.springframework.web.reactive.socket.WebSocketSession
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
 import reactor.core.publisher.Flux
+import reactor.core.publisher.FluxSink
 import reactor.core.publisher.SynchronousSink
+import java.io.File
 import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
 import java.util.function.Consumer
 
 @SpringBootApplication
@@ -23,6 +34,21 @@ fun main(args: Array<String>) {
 
 @Configuration
 class WebSocketConfiguration {
+
+    @Bean
+    fun incomingFileFlow(@Value("file://\${HOME}/Desktop/in") f: File) =
+            IntegrationFlows
+                    .from(
+                            Files
+                                    .inboundAdapter(f)
+                                    .autoCreateDirectory(true),
+                            { p -> p.poller({ pm -> pm.fixedRate(1000) }) })
+                    .channel(incomingFilesChannel())
+                    .get()
+
+    @Bean
+    fun incomingFilesChannel() = PublishSubscribeChannel()
+
     @Bean
     fun wsha() = WebSocketHandlerAdapter()
 
@@ -30,12 +56,12 @@ class WebSocketConfiguration {
     fun hm(): HandlerMapping {
         val suhm = SimpleUrlHandlerMapping()
         suhm.order = 10
-        suhm.urlMap = mapOf("/ws/files" to wsh())
+        suhm.urlMap = mapOf("/ws/time" to webSocketHandlerTime(), "/ws/files" to wsh())
         return suhm
     }
 
     @Bean
-    fun wsh(): WebSocketHandler {
+    fun webSocketHandlerTime(): WebSocketHandler {
         return WebSocketHandler { session ->
             val om = ObjectMapper()
             val publisher = Flux
@@ -49,6 +75,40 @@ class WebSocketConfiguration {
         }
     }
 
+
+    @Bean
+    fun wsh(): WebSocketHandler {
+        val om = ObjectMapper()
+        val connections = ConcurrentHashMap<String, MessageHandler?>()
+
+        class ForwardingMessageHandler(val session: WebSocketSession,
+                                       val sink: FluxSink<WebSocketMessage>) : MessageHandler {
+            private val sessionId = session.id
+
+            override fun handleMessage(msg: Message<*>) {
+                val payload = msg.payload as File
+                val fe = FileEvent(sessionId = sessionId, path = payload.absolutePath)
+                val str = om.writeValueAsString(fe)
+                val tm = session.textMessage(str)
+                sink.next(tm)
+
+            }
+        }
+
+
+        return WebSocketHandler { session ->
+            val publisher = Flux
+                    .create(Consumer<FluxSink<WebSocketMessage>> { sink ->
+                        connections[session.id] = ForwardingMessageHandler(session, sink)
+                        connections[session.id]?.let { incomingFilesChannel().subscribe(it) }
+                    })
+                    .doFinally {
+                        connections[session.id]?.let { it1 -> incomingFilesChannel().unsubscribe(it1) }
+                        connections.remove(session.id)
+                    }
+            session.send(publisher)
+        }
+    }
 }
 
-data class FileEvent(val sessionId: String, val path: String)
+    data class FileEvent(val sessionId: String, val path: String)
